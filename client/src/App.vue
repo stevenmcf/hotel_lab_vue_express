50d157111806741940fd581752ac46142814c8b9
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-10-28 13:22:39 +0000

    fetch working in vue devtools - woo hoo

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,39 @@
-<template>
-  
+<template lang='html'>
+  <h1>Welcome to Hotel California</h1>
 </template>
 
 <script>
-export default {
+import BookingService from './services/BookingService';
 
+export default {
+  name: 'app',
+  
+  data() {
+    return {
+      bookings: []
+    };
+  },
+
+  mounted() {
+    this.fetchBookings();
+  },
+
+  methods: {
+    fetchBookings() {
+      BookingService.getBookings()
+      .then(bookings => this.bookings = bookings);
+    }
+  }
 }
+
+
 </script>
 
-<style>
+<style scoped>
+
+#app {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+}
+
 
 </style>
