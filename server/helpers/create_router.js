e9b70bcac5a34b70c0de2f164be5a7f23418eaec
stmcf <stevenmcfarlane@hotmail.co.uk>
2020-10-28 11:46:19 +0000

    full CRUD functionality any persistence with db working A OK.

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -17,8 +17,47 @@ const createRouter = function (collection) {
         });
     });
 
+    router.get('/:id', (req, res) => {
+        const id = req.params.id;
+        collection
+        .findOne({ _id: ObjectID(id )})
+        .then((doc) => res.json(doc))
+        .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+    });
+});
+
+    router.delete('/:id', (req, res) => {
+        const id = req.params.id;
+        collection
+        .deleteOne({ _id: ObjectID(id) })
+        .then(() => collection.find().toArray())
+        .then((docs) => res.json(docs))
+        .catch((err) => {
+            console.error(err);
+            res.status(500);
+            res.json({ status: 500, error: err });
+        });
+    });
+
+    router.post('/', (req, res) => {
+       const newData = req.body;
+       collection 
+       .insertOne(newData)
+       .then((result) => {
+           res.json(result.ops[0])
+       })
+       .catch((err) => {
+        console.error(err);
+        res.status(500);
+        res.json({ status: 500, error: err });
+    });
+    })
+
     return router;
 
-}
+};
 
 module.exports = createRouter;
